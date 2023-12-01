9c9822c3cd3bf60409b7df63f759a2d11788e302
lucaaspw <gonclaves.10.lucas@gmail.com>
2023-12-01 19:50:32 -0300

    Project Dashboard in Nextjs

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Lusitana, Nunito } from 'next/font/google';
+
+export const inter = Inter({ 
+  subsets: ['latin']
+});
+export const lusitana = Lusitana({
+  weight : ['400','700'],
+  subsets: ['latin']
+});
+
+export const nunito = Nunito({
+  weight: ['400', '900'],
+  subsets: ['latin']
+})
