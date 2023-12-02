737550417f417b7ccae83693341174dda763e638
lucaaspw <gonclaves.10.lucas@gmail.com>
2023-12-02 12:38:13 -0300

    Text alto modifier

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -28,7 +28,10 @@ export default async function CustomersTable({
                       <div>
                         <div className="mb-2 flex items-center">
                           <div className="flex items-center gap-3">
-                            <Image alt={`I's profile picture`} src={customer.image_url} className="rounded-full" 
+                            <Image
+                              alt={`${customer.name}'s profile picture`}
+                              src={customer.image_url}
+                              className="rounded-full"
                               width={28}
                               height={28}
                             />
@@ -83,9 +86,9 @@ export default async function CustomersTable({
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
                           <Image
+                            alt={`${customer.name}'s profile picture`}
                             src={customer.image_url}
                             className="rounded-full"
-                            alt={`${customer.name}'s profile picture`}
                             width={28}
                             height={28}
                           />
